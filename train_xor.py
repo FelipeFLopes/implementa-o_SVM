b03d9b089f4583f9c4cc636a94ff4dce2ad6d624
Felipe Lopes <felipe.lopes@dca.ufrn.br>
2020-11-27 23:00:46 -0300

    chg: Refactor xor training script

--- train_xor.py
+++ train_xor.py
@@ -5,78 +5,46 @@ from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.linear_model import SGDClassifier
 
 
-def main():
+def generate_xor_experiment():
     set_random_seed()
 
-    s = generate_elements_xor(2000)
+    xor_dataset = make_xor_dataset(2000)
 
-    xor_dataset = make_xor_dataset(s)
+    samples = xor_dataset["samples"]
+    centers = generate_centers(samples, n_clusters=7)
+    kernelized_inputs = apply_rbf_kernel(samples, centers, gamma=5)
 
-    centers = generate_centers(xor_dataset["samples"], n_clusters=7)
-
-    kernelized_inputs = apply_rbf_kernel(
-        xor_dataset["samples"], centers, gamma=5)
-
-    xor_dataset["samples"] = kernelized_inputs
-
-    X = xor_dataset["samples"]
-    y = xor_dataset["classes"]
-
-    model = _create_sgd_classifier()
+    model = create_sgd_classifier(alpha=0.001, learning_rate=0.7)
 
     n_iter = 10
+    y = xor_dataset["classes"]
 
     weights_history, pred_history = partial_weights_and_prediction(
-        xor_dataset["samples"], xor_dataset["classes"], model, n_iter)
+        kernelized_inputs, y, model, n_iter)
 
-    final_weights = obtain_weights_and_bias(model)
+    save_array_csv(centers, file_name="centers_xor.csv")
+    save_array_csv(weights_history, file_name="weight_history_xor.csv")
+    save_array_csv(pred_history, file_name="pred_history_xor.csv")
 
-    save_array_csv(final_weights, file_name="centers.csv",
-                   header="bias, dim1, dim2, dim3, dim4, dim5")
-
-    save_array_csv(np.asarray(weights_history), file_name="weight_history.csv",
-                   header="bias, dim1, dim2, dim3, dim4, dim5")
-
-    print(f"final accuracy was {model.score(X, y)}")
+    print(f"final accuracy was {model.score(kernelized_inputs, y)}")
 
 
 def set_random_seed(seed=42):
     np.random.seed(seed)
 
 
-def plot_dataset_2d(samples, classes):
-    x = samples[:, 0]
-    y = samples[:, 1]
-    colors = classes
-
-    plt.scatter(x, y, c=colors)
-    plt.show()
+def make_xor_dataset(n_samples):
+    samples = _generate_elements_xor(n_samples)
+    classes = _xor(samples)
+    samples_with_classes = {"samples": samples, "classes": classes}
+    return samples_with_classes
 
 
-def plot_dataset_3d(samples, classes):
-    x = samples[:, 0]
-    y = samples[:, 1]
-    z = samples[:, 2]
-    colors = classes
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.scatter(x, y, z, c=colors)
-    plt.show()
-
-
-def generate_elements_xor(n_samples=10):
+def _generate_elements_xor(n_samples=10):
     X = np.random.uniform(-1, 1, n_samples).reshape(-1, 1)
     Y = np.random.uniform(-1, 1, n_samples).reshape(-1, 1)
     samples = np.concatenate((X, Y), axis=1)
     return samples
-
-
-def make_xor_dataset(samples):
-    classes = _xor(samples)
-    samples_with_classes = {"samples": samples, "classes": classes}
-    return samples_with_classes
 
 
 def _xor(samples):
@@ -107,9 +75,9 @@ def apply_rbf_kernel(samples, centers, gamma=0.1):
     return result
 
 
-def _create_sgd_classifier():
-    model = SGDClassifier(loss="hinge", penalty="l2", alpha=0.2,
-                          learning_rate="constant", eta0=0.4,
+def create_sgd_classifier(alpha=0.0, learning_rate=0.7):
+    model = SGDClassifier(loss="hinge", penalty="l2", alpha=alpha,
+                          learning_rate="constant", eta0=learning_rate,
                           fit_intercept=True, shuffle=False)
     return model
 
@@ -141,10 +109,32 @@ def obtain_weights_and_bias(model):
     return np.concatenate((model.intercept_, model.coef_[0]), axis=0)
 
 
-def save_array_csv(array, file_name="data.csv", header="A,B,C"):
+def save_array_csv(array, file_name="data.csv", header=""):
     np.savetxt(file_name, array, delimiter=',',
                header=header, comments="")
 
 
+def plot_dataset_2d(samples, classes):
+    x = samples[:, 0]
+    y = samples[:, 1]
+    colors = classes
+
+    plt.scatter(x, y, c=colors)
+    plt.show()
+
+
+def plot_dataset_3d(samples, classes):
+    x = samples[:, 0]
+    y = samples[:, 1]
+    z = samples[:, 2]
+    colors = classes
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(x, y, z, c=colors)
+    plt.show()
+
+
 if __name__ == "__main__":
-    main()
+    generate_xor_experiment()
